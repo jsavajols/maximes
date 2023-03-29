6a13aabaa5d569fd61bc88d9292b3781b2029907
jsavajols@delta-assurances.fr <jsavajols@gmail.com>
2023-03-29 16:00:54 +0200

    custom desing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,47 @@
 <script>
-    import  Wait  from '../components/wait/+Wait.svelte';
-	import { page } from '$app/stores';
+	import Wait from "../components/wait/+Wait.svelte";
+	import { page } from "$app/stores";
 	let count = 1;
-	let author="";
-	let maxime="";
-    let waitVisible = false;
-	let fetchMaxime = (async () => {
-        waitVisible = true;
-		let url = $page.url + 'api/maxims/random';
+	let author = "";
+	let maxime = "";
+	let waitVisible = false;
+	let fetchMaxime = async () => {
+		waitVisible = true;
+		let url = $page.url + "api/maxims/random";
 		let response = await fetch(url);
 		let rep = await response.json();
 		rep = rep[0];
 		author = "<b>Auteur</b> : " + rep.author;
-		maxime = "<b>Maxime n° " + count  + " : </b>" + rep.maxim;
+		maxime = "<b>Maxime n° " + count + " : </b>" + rep.maxim;
 		count++;
-        waitVisible = false;
-	})
+		waitVisible = false;
+	};
 </script>
 
-<div>
-	<h1>Une maxime</h1>
-    <Wait isVisible={ waitVisible }/>
-	{ @html author }<br>
-	{ @html maxime }
+<div class="page">
+	<div class="page">
+		<img src="1clusif-logo-vertical.png" alt="Les maximes" width="30%" />
+		<h1>Une maxime</h1>
+		<Wait isVisible={waitVisible} />
+		{@html author}<br />
+		{@html maxime}
+	</div>
+
+	<div>
+		<button class="myButton" disabled={waitVisible} on:click={fetchMaxime}
+			>{!waitVisible ? "Une nouvelle maxime ?" : "Patientez..."}</button
+		>
+	</div>
 </div>
 
-<div>
-	<button disabled = { waitVisible } on:click={fetchMaxime}>{(!waitVisible) ? 'Une nouvelle maxime ?' : 'Patientez...'}</button>
-</div>
-
+<style>
+	.page {
+		padding: 1em;
+		max-width: 600px;
+		margin: 0 auto;
+	}
+	.myButton {
+		margin-top: 20px;
+		color: brown;
+	}
+</style>
